8cd79e83dffc9014fa51accc2d713954d022efbf
Yilin Hu <hu10xiaorui@outlook.com>
2020-01-01 16:43:15 -0800

    made this game a stupid html game, add buy menu

--- main.py
+++ main.py
@@ -1,51 +1,62 @@
+def buymenu():
+    tk = tkinter.Tk()
+    ctext=tkinter.StringVar()
+    bg = tkinter.PhotoImage(file = 'bg.gif')
+    frame = tkinter.Frame(tk, relief=RIDGE, borderwidth=2, image=bg)
+    frame.pack(fill=BOTH,expand=1)
+    label = tkinter.Label(frame, text='CLICK ME 2 BUY STUFF')
+    label.pack(fill=X, expand=1)
+    button = tkinter.Button(frame,text='''click me''',image=cookie,command=add1cookie)
+    button.pack(side=BOTTOM)
+    buyb = tkinter.Button(frame,text='''BUY STUFF''',command=buymenu)
+    buyb.pack(side=BOTTOM)
+    tk.mainloop()
+
 def add1cookie():
     global lc
     global label
     global c
     if c==0:
-        pass
+
     else:
         tu=time.time()-lc
         if tu<0.04:
             cheatedected()
         if c%10==0:
-            cheatdectetion()
+            #playsound.playsound('oven_door.mp3',False)
+
     c=c+1
-    ctext.set("Cookie(s):"+str(c))
+    ctext.set('Cookie(s):'+str(c))
+    lc=time.time()
 
 
-def cheatdectetion():
-    print("decting cheats")
-    apps=(p.name() for p in psutil.process_iter())
-    for cexe in cl:
-        if cexe in apps:
-            cheatedected()
 def cheatedected():
-
+    global c
     if c==0:
-        pass
+
     else:
         c=0
-        ctext.set("CHEAT DETECTED! PLEASE QUIT YOUR AUTOCLICKER")
-        time.sleep("30")
+        ctext.set('CHEAT DETECTED! PLEASE QUIT YOUR AUTOCLICKER')
+        time.sleep('30')
         quit()
 
-import tkinter
-import time
-from tkinter.constants import *
-import psutil
+var tkinter = require('tkinter')
+var playsound = require('playsound')
+var time = require('time')
+from tkinter.constants var * = require('*')
 lc=time.time()
 c=0
-cl=["MurGaa Auto Clicker","Auto Mouse Click","Mac Auto Clicker","AutoClicker.exe","AutoClickers.exe","FreeAutoClicker.exe","GSAutoClicker.exe"]
 tk = tkinter.Tk()
 ctext=tkinter.StringVar()
 
 
-cookie = tkinter.PhotoImage(file = "cookie_ss.gif")
+cookie = tkinter.PhotoImage(file = 'cookie_ss.gif')
 frame = tkinter.Frame(tk, relief=RIDGE, borderwidth=2)
 frame.pack(fill=BOTH,expand=1)
 label = tkinter.Label(frame, textvariable=ctext)
 label.pack(fill=X, expand=1)
 button = tkinter.Button(frame,text='''click me''',image=cookie,command=add1cookie)
 button.pack(side=BOTTOM)
+buyb = tkinter.Button(frame,text='''BUY STUFF''',command=buymenu)
+buyb.pack(side=BOTTOM)
 tk.mainloop()
